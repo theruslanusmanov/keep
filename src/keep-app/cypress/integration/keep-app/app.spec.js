72d6496cdacc071fdff53e2a9f184f5a55495c00
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-03-15 15:14:59 +0500

    Update tests

--- src/keep-app/cypress/integration/keep-app/app.spec.js
+++ src/keep-app/cypress/integration/keep-app/app.spec.js
@@ -1,63 +1,60 @@
-const APP_URL = 'https://localhost:3000';
+const APP_URL = 'https://localhost:3000'
 
 describe('App', () => {
 
   beforeEach(() => {
-    cy.visit(APP_URL);
-  });
+    cy.visit(APP_URL)
+  })
 
-  describe('Notes', function() {
+  describe('Notes', function () {
 
-    it('should create note and update list', function() {
-      cy.get('[data-cy="notes"]').should('have.length', 2);
-      cy.get('[data-cy="create-button"]').click();
+    it('should create note and update list', function () {
+      cy.get('[data-cy="notes"]').should('have.length', 2)
+      cy.get('[data-cy="create-button"]').click()
 
-      const bodyText = 'New note text.';
+      const bodyText = 'New note text.'
       cy.intercept({
         method: 'POST', url: '**/note',
       }, {
         statusCode: 200, body: bodyText,
-      }).as('createNote');
-      cy.get('[data-cy="note-body"]').type(`${bodyText}{enter}`);
-      cy.wait('@createNote').should(({request, response}) => {
-        expect(request && request.body).to.have.value(bodyText);
-      });
+      }).as('createNote')
+      cy.get('[data-cy="note-body"]').type(`${bodyText}{enter}`)
+      cy.wait('@createNote').should(({ request, response }) => {
+        expect(request && request.body).to.have.value(bodyText)
+      })
 
-      cy.get('[data-cy="notes"]').should('have.length', 3);
-    });
+      cy.get('[data-cy="notes"]').should('have.length', 3)
+    })
 
-    it('should show notes list', function() {
+    it('should show a list of notes', function () {
+      const notes = [
+        { id: '1', body: 'Note' },
+        { id: '2', body: 'Another Note' },
+      ];
       cy.intercept({
         method: 'POST', url: '**/notes',
       }, {
         statusCode: 200,
-        body: [
-          {id: '1', body: 'Note'},
-          {id: '2', body: 'Another Note'},
-        ],
-      }).as('notes');
-      cy.get('[data-cy="notes"]').should('have.length', 2);
-    });
+        body: notes,
+      }).as('notes')
+      cy.wait('@notes').should(({ request, response }) => {
+        expect(response.statusCode).should('be', 200)
+        expect(response && response.body).to.have.value(notes)
+      })
+      cy.get('[data-cy="notes"]').should('have.length', 2)
+    })
 
-    it('should pin/unpin a note', function() {});
-
-    describe('Tags', function() {
-
-      it('should add/remove tag to note', function() {});
-
-      it('should filter notes by tag', function() {});
-    });
-  });
-
-  describe('Bin', function() {
-
-    it('should add note to bin', function() {});
-
-    it('should empty bin', function() {});
-
-    it('should restore note from bin', function() {});
-  });
-
-
-  it('should switch dark/light theme', function() {});
-});
+    it('should remove note', function () {
+      const noteId = 'note_id'
+      cy.intercept({
+        method: 'DELETE', url: '**/note',
+      }, {
+        statusCode: 200, body: noteId,
+      }).as('deleteNote')
+      cy.get('[data-cy="notes"] > [data-cy="note"]:first-child')
+        .find('[data-cy="delete-button"]')
+        .click()
+      cy.wait('@deleteNote').its('response.statusCode').should('be', 200)
+    })
+  })
+})
