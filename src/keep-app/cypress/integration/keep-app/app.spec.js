2960f1be03caaabca4e22fc93f0d069082f2e84b
Ruslan Usmanov <ruslanusm6@gmail.com>
2022-03-15 11:22:42 +0500

    create app spec

---
+++ src/keep-app/cypress/integration/keep-app/app.spec.js
@@ -0,0 +1,18 @@
+describe('App', () => {
+
+  it('should create note and update list', function() {});
+
+  it('should show notes list', function() {});
+
+  it('should remove note, add it to bin', function() {});
+
+  it('should restore note from bin', function() {});
+
+  it('should add/remove tag to note', function() {});
+
+  it('should filter notes by tag', function() {});
+
+  it('should pin/unpin a note', function() {});
+
+  it('should switch dark/light theme', function() {});
+});
